cc84440e0a0cab54c3274e4e317227fb6672c38a
rahul0901 <rahul.shinde0904@gmail.com>
2024-01-29 18:23:59 +0530

    addd

---
+++ listrace/src/assets/common.js
@@ -0,0 +1,26 @@
+let bannerImg = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/banner.jpg";
+
+
+let mountainImg = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/counter-banner.jpg";
+
+let cafeImage = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e1.jpg";
+
+let beachImg = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e2.jpg";
+
+let mountainHouse = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e3.jpg";
+
+let swimmingPoolVilla = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e4.jpg";
+
+let vintageCar = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e5.jpg";
+
+let trekkingImg = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/e6.jpg";
+
+let monumentLakeImg = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/b1.jpg";
+
+let smallGirl = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/b2.jpg";
+
+let cityRideBike = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/b3.jpg";
+
+let blueShirt = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/c2.png";
+
+let navyBlueShirt = "https://www.free-css.com/assets/files/free-css-templates/preview/page296/listrace/assets/images/c4.png";
