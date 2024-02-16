10ea3d98eafe3fdf9a689e64d572843f94bf0eed
rahul0901 <rahul.shinde0904@gmail.com>
2024-02-17 03:03:43 +0530

    addd

---
+++ glintwebsiteui/src/Components/Works.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Works.css';
+import './MediaWorks.css';
+
+const Works = () => {
+    return (
+        <>
+            <div className="screen-work" id='work'>
+                <div className="work-parent">
+                    <div className="work-head">
+                        <span>RECENT WORKS</span>
+                        <h2>We love what we do, check out some of our latest works</h2>
+                    </div>
+                </div>
+                <div className="work-dummy-parent">
+                    <div className="work-image-parent">
+                        image
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Works;
