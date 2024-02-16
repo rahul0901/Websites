0fb3d117a969629f8bc8197b8bd3eb5aa48a5f9d
rahul0901 <rahul.shinde0904@gmail.com>
2024-02-17 03:04:10 +0530

    adddd

---
+++ listrace/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='screen-home'>
+      Home
+    </div>
+  )
+}
+
+export default Home;
---
+++ listrace/src/Pages/HowItWorks.jsx
@@ -0,0 +1,9 @@
+import './HowItWorks.css';
+
+const HowItWorks = () => {
+  return (
+    <div>HowItWorks</div>
+  )
+}
+
+export default HowItWorks;
